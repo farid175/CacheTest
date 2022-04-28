cfff0cbdae2ef31fa6ad065e6f6b57a828d0baab
= <=>
2022-04-28 17:48:26 +0400

    initial

---
+++ IDistributedCaheApp/Controllers/ProductController.cs
@@ -0,0 +1,73 @@
+﻿using IDistributedCaheApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IDistributedCaheApp.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private IDistributedCache _distributedCache;
+
+        public ProductController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public IActionResult Index()
+        {
+            DistributedCacheEntryOptions options = new DistributedCacheEntryOptions();
+            options.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+
+            Product product = new Product {Id = 1,Name = "Phone", Price = 1000 };
+
+            string jsonobject = JsonConvert.SerializeObject(product);
+
+            _distributedCache.SetString("name", "Qurban",options);
+            _distributedCache.SetString("product", jsonobject,options);
+
+            return View();
+        }
+        
+        public IActionResult Show()
+        {
+            var product = _distributedCache.GetString("product");
+            Product p = JsonConvert.DeserializeObject<Product>(product);
+
+            ViewBag.p = p;
+            ViewBag.name = _distributedCache.GetString("name");
+            return View();
+        }
+
+        public IActionResult Remove()
+        {
+            _distributedCache.Remove("name");
+            return View();
+        }
+
+        public IActionResult ImageCache()
+        {
+            DistributedCacheEntryOptions options = new DistributedCacheEntryOptions();
+            options.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+
+            string path = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/images/download.jpg");
+            Byte[] imageByte = System.IO.File.ReadAllBytes(path);//butun pdf image her weyi byte cevirib cachelemek olar
+
+            _distributedCache.Set("wekil",imageByte,options);
+            return View();
+        }
+
+        public IActionResult ImageUrl()
+        {
+            Byte[] wekilByte = _distributedCache.Get("wekil");
+
+            return File(wekilByte,"image/jpg");
+        }
+    }
+}
---
+++ InMemoryApp/Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using InMemoryApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InMemoryApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IMemoryCache _memoryCache;
+
+
+        public ProductController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public IActionResult Index()
+        {
+            MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+            options.AbsoluteExpiration = DateTime.Now.AddMinutes(1); // deqiq vaxt
+            options.SlidingExpiration = TimeSpan.FromSeconds(10); // burda ise 10 saniye erzinde sorgu gelse yene 10 saniye artyir gelmese silinir
+            options.Priority = CacheItemPriority.High; // priorityni high verende yeni en son bunu sil
+            options.RegisterPostEvictionCallback((key, value, reason,state) =>
+            {
+                _memoryCache.Set("callback", $"key:{key} value:{value} reason:{reason}");
+            });
+            _memoryCache.Set<string>("zaman",DateTime.Now.ToString(),options);
+            _memoryCache.Set<Product>("product",new Product {Id = 1,Name = "Notebook",Stock = 100 });
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            _memoryCache.TryGetValue<string>("zaman", out string zaman);
+            _memoryCache.TryGetValue<string>("callback", out string callback);
+            _memoryCache.TryGetValue<Product>("product", out Product product);
+            ViewBag.zaman = _memoryCache.Get<string>("zaman");
+            ViewBag.callback = _memoryCache.Get<string>("callback");
+            ViewBag.product = _memoryCache.Get<Product>("product");
+            
+            
+            return View();
+        }
+    }
+}
