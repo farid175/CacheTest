867521813576dc4c56e0359627d6b3a4cd376bb8
= <=>
2022-04-29 12:21:42 +0400

    StackExchange.RedisApi

---
+++ RedisExchangeApiApp/Services/RedisService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisExchangeApiApp.Services
+{
+    public class RedisService
+    {
+        public RedisService(IConfiguration configuration)
+        {
+
+        }
+    }
+}
